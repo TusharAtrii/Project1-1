5fc0381fee10abef87523e78f3940aa37d825ad2
TusharAtrii <tushar@prologictechnologies.in>
2023-11-16 19:08:30 +0530

    changiing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./App.css";
 import SignUpComp from "./views/components/Loginn";
 import MainPage from "./views/pages/MainPage";
 
-
 // import OtpVerification from "./views/components/OtpVerification";
 
 // import OtpVerification from "./views/components/OtpVerification";
@@ -12,12 +11,9 @@ import MainPage from "./views/pages/MainPage";
 // import OtpVerification from "./views/components/OtpVerification";
 // import MainPage from "./views/pages/MainPage";
 
-import Home from "./views/components/Home";
-import MainPage from "./views/pages/MainPage";
 function App() {
   // return <OtpVerification />
   return <MainPage />;
 }
-
 
 export default App;
